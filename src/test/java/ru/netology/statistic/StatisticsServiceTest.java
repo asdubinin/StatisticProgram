13aceaf494e946340a3eec76c165f8da7964a05f
Alexander <asdubinin14@gmail.com>
2023-02-02 17:36:00 +0600

    Add test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,15 @@ public class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+    @Test
+    void findMaxIfFirstLowerSecond() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {1, 10, 6, 5, 8, 4, 7};
+        long expected = 10;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
 }
